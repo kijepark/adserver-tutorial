ada554114c729722212b57459a1b1ae90fa5e330
kijepark <kije.park@yahoo.com>
2020-08-16 21:47:19 +0900

    Add controller Placement

---
+++ src/controllers/placement.js
@@ -0,0 +1,28 @@
+var Placement = require("./../models/placement");
+
+module.exports = {
+  list: function(query) {
+    return new Promise(function(resolve, reject) {
+      Placement.find(query)
+      .then(function(res) {
+        if (res) console.log({ query }, "Lists Placements");
+        return resolve(res);
+      })
+      .catch(function(error) {
+        return reject(error);
+      });
+    });
+  },  
+  create: function(query) {
+    return new Promise(function(resolve, reject) {
+      Placement.create(query)
+      .then(function(res) {
+        if (res) console.log({ query }, "Creates Placement");
+        return resolve(res);
+      })
+      .catch(function(error) {
+        return reject(error);
+      });
+    });
+  }
+}
