60921192670a763552227964eb66798247e0dc00
kijepark <kije.park@yahoo.com>
2020-08-16 21:03:25 +0900

    Add controller AdItem

---
+++ src/controllers/adItem.js
@@ -0,0 +1,28 @@
+var AdItem = require("./../models/adItem");
+
+module.exports = {
+  list: function(query) {
+    return new Promise(function(resolve, reject) {
+      AdItem.find(query)
+      .then(function(res) {
+        if (res) console.log({ query }, "Lists AdItems");
+        return resolve(res);
+      })
+      .catch(function(error) {
+        return reject(error);
+      });
+    });
+  },
+  create: function(query) {
+    return new Promise(function(resolve, reject) {
+      AdItem.create(query)
+      .then(function(res) {
+        if (res) console.log({ query }, "Creates AdItem");
+        return resolve(res);
+      })
+      .catch(function(error) {
+        return reject(error);
+      });
+    });
+  }
+}
