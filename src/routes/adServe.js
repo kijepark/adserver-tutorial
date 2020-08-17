0f4cb3f0015f993446b9861a98206fd1f9262328
kijepark <kije.park@yahoo.com>
2020-08-17 15:25:15 +0900

    Add JavaScript type delivery method

--- src/routes/adServe.js
+++ src/routes/adServe.js
@@ -10,16 +10,14 @@ var router = express.Router();
 
 router.get("/adserve", async function(req, res) {
   try {
-    var sizes = req.query.size.split("x");
-    var zoneWidth = parseInt(sizes[0]);
-    var zoneHeight = parseInt(sizes[1]);
+    var type = req.query.type;
     var zoneID = req.query.zone_id;
 
     var zone = await Zone.retrieve({ id: zoneID });
     if (!zone) {
       return res.send("No Zone found");
     }
-    
+
     var placements = await Placement.list({ "zone.id": zone.id });
     if (!placements.length) {
       return res.send("No Placements found");
@@ -30,7 +28,7 @@ router.get("/adserve", async function(req, res) {
     // but in this tutorial I didn't include kind of options
     // get random one instead
     var placement = placements[Math.floor(Math.random() * placements.length)];
-    
+
     var campaign = await Campaign.retrieve({ id: placement.advertisement.id });
     if (!campaign) {
       return res.send("No Campaign Found");
@@ -44,7 +42,7 @@ router.get("/adserve", async function(req, res) {
       var adItem = await AdItem.retrieve({ id: campaignAssignment.advertisement.id });
 
       // Push only ad that has same dimension with Zone's
-      if (zoneWidth === adItem.width && zoneHeight === adItem.height) {
+      if (zone.width === adItem.width && zone.height === adItem.height) {
         adItems.push(adItem);
       }
     }
@@ -52,15 +50,38 @@ router.get("/adserve", async function(req, res) {
     // Also need to figure out the most eligible one but
     // use Random() instead
     var adItem = adItems[Math.floor(Math.random() * adItems.length)];
-    var data = {
-      width: adItem.width,
-      height: adItem.height,
-      target: adItem.html_target,
-      redirect_url: adItem.location,
-      image_url: adItem.creative_url
-    }
+    var response = null;
 
-    return res.send(data);
+    switch(type) {
+      case "js": {
+        response = "";
+        response += 'document.write(\'<div style="display:inline-block;margin:0;padding:0;">\');';
+        response += 'document.write(\'';
+        response +=   '<a href="' + adItem.location + '" target="' + adItem.html_target + '" rel="nofollow">';
+        response +=   '<img src="' + adItem.creative_url + '" border="0" width="' + adItem.width + '" height="' + adItem.height + '">';
+        response += '\');';
+        response += 'document.write(\'</div>\');';
+
+        res.setHeader("Content-Type", "text/plain");
+        res.end(response);
+        return;
+      }
+      case "iframe": {
+
+      }
+      case "json": {
+        response = {
+          width: adItem.width,
+          height: adItem.height,
+          target: adItem.html_target,
+          redirect_url: adItem.location,
+          image_url: adItem.creative_url
+        }
+
+        res.send(response);
+        return;
+      }
+    }
   }catch(error) {
     return res.send(error);
   }
